56453bd22882cdd15b50263eb995842f638f982a
Nurkassym03 <119846588+Nurkassym03@users.noreply.github.com>
2022-12-23 20:51:47 +0600

    Add files via upload

---
+++ Lyrics.java
@@ -0,0 +1,28 @@
+package genius;
+
+//import genius.LyricsParser;
+import Main.GLA;
+
+public class Lyrics {
+
+    private final GLA gla;
+    private final String path;
+    private final String id;
+
+    public Lyrics(GLA gla, String id, String path) {
+        this.path = path;
+        this.gla = gla;
+        this.id = id;
+    }
+
+    public String getText() {
+        return new LyricsParser(this.gla).get(this.id);
+    }
+
+    @Override
+    public String toString() {
+        return this.path;
+    }
+
+}
+
